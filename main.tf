3cc838ba6227ba2aeba867efa7c80be3ca613d61
ram dv <dvr.ops@gmail.com>
2023-03-02 05:54:51 +0530

    first terraform script to create rg and storge account

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "azurerm" {
+  features {}
+  storage_use_azuread = true
+  }
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "my-resource-group"
+  location = "eastus"
+}
+
+# Create a storage account
+resource "azurerm_storage_account" "st" {
+  name                     = "mystaccount127902032023"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
